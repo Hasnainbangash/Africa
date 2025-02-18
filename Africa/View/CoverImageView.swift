314d86e7e360d1b6401730a7bf5023db47e3ba17
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 15:53:29 +0500

    added a newb file cover image view along with the comments

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,23 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    CoverImageView()
+}
