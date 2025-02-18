8e98b393517b7decc0132d7ca413bb8b63ba7e37
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 15:29:47 +0500

    New swift Ui file is added to the project along with some changesb

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+#Preview {
+    VideoListView()
+}
