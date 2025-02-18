dc62c38bf81f23e5afa5d41bc6326bf2c2ae8b5c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 15:32:21 +0500

    Gallery view file is added along with the mainview file

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ Africa/App/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
