540464d8bee3e169f07739e9d9e4d366df94e3e4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 15:30:54 +0500

    New file map view is added

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}
