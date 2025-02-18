ce6e4dad246aacda2858c64acfc17ac6610724da
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 16:35:31 +0500

    An extension of bundle  for decodable empty function is crated alon with the comments

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,27 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the json file
+        
+        
+        // 2. Create a property for the data
+        
+        
+        // 3. Create a decoder
+        
+        
+        // 4. Create a property for the decoded data
+        
+        
+        // 5. Return the ready to use data
+        
+    }
+}
