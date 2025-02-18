438edff33e972207d0a6bd58c4ce481b1baf9992
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-18 15:11:46 +0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Elexoft on 18/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
