6a375cae65b9e8ec3a1e95c1b8f33d759101e546
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 15:27:57 +0500

    Vdideo player view little bit work done

--- Africa/View/VideoPlayerView.swift
+++ Africa/View/VideoPlayerView.swift
@@ -11,15 +11,35 @@ import AVKit
 struct VideoPlayerView: View {
     // MARK: - PROPERTIES
     
+    var videoSelected: String
+    var videoTitle: String
+    
     // MARK: - BODY
     
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "cheetah", withExtension: "mp4")!))
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+                Text(videoTitle)
+            }
+            .overlay (
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8)
+                , alignment: .topLeading
+            )
+        } //: VSTACK
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
     }
 }
 
 // MARK: - PREVIEW
 
 #Preview {
-    VideoPlayerView()
+    NavigationView {
+        VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+    }
 }
