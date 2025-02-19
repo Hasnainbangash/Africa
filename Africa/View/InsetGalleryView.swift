4b985aa10eaef7ec9914f760334ef0840f708126
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 11:43:17 +0500

    inset gallery view work done of animal deatil screen and work done

--- Africa/View/InsetGalleryView.swift
+++ Africa/View/InsetGalleryView.swift
@@ -10,16 +10,31 @@ import SwiftUI
 struct InsetGalleryView: View {
     // MARK: - PROPERTIES
     
+    let animal: Animal
+    
     // MARK: - BODY
     
     var body: some View {
-        
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 15) {
+                ForEach(animal.gallery, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                        .cornerRadius(12)
+                } //: LOOP
+            } //: HSTACK
+        } //: SCROLL
     }
 }
 
 // MARK: - PREVIEW
 
 #Preview {
-    InsetGalleryView()
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    InsetGalleryView(animal: animals[0])
+        .previewLayout(.sizeThatFits)
+        .padding()
 }
