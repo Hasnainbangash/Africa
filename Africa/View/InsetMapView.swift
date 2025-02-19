8e6c3b725cd8fcf3e70cbb00c4efb0836603c4c4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 12:26:19 +0500

    New file is added inset map view and the map kit is get imported to the project

---
+++ Africa/View/InsetMapView.swift
@@ -0,0 +1,28 @@
+//
+//  InsetMapView.swift
+//  Africa
+//
+//  Created by Elexoft on 19/02/2025.
+//
+
+import SwiftUI
+import MapKit
+
+struct InsetMapView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    InsetMapView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
