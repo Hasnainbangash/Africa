633c98931bf30f59f7a58aabc14e96d9bec7c786
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 11:24:00 +0500

    Animal detail view image and titlte is added t the screen

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -18,10 +18,25 @@ struct AnimalDetailView: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
                 // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
                 
                 // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.secondary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
                 
                 // HEADLINE
+                
                 
                 // GALLERY
                 
