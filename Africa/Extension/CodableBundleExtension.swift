0a8d7744d8080ed80bc8107c1215a8580df6cfdc
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 10:45:45 +0500

    Generics are implemenetd in the decode function

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -8,6 +8,34 @@
 import Foundation
 
 extension Bundle {
+    // Decoding the data using the generics for every json file
+    func decode<T: Codable>(_ file: String) -> T {
+        // 1. Locate the json file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for the decoded data
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        // 5. Return the ready to use data
+        return loaded
+    }
+}
+
+/*
+extension Bundle {
+    // Decoding the data using the simple way for only one json file
     func decode(_ file: String) -> [CoverImage] {
         // 1. Locate the json file
         guard let url = self.url(forResource: file, withExtension: nil) else {
@@ -31,3 +59,4 @@ extension Bundle {
         return loaded
     }
 }
+*/
