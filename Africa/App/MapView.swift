cc8b072bf93f4380e92039551713394bbbe28c47
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 16:24:28 +0500

    old style pin is added and ndewstyle pin is added

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Map")
+        // MARK: - NO1 BASIC MAP
+        // Map(coordinateRegion: $region)
+        
+        // MARK: - NO2 ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE (always static)
+            // MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) PIN: NEW STYLE (always static)
+            MapMarker(coordinate: item.location, tint: .accentColor)
+        })
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     MapView()
