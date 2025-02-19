a4c694bf49a909f3796683ed0da74dc5c4321eb0
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 15:16:37 +0500

    Vieo player helper is ready to be used and coding work done

---
+++ Africa/Utilities_Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Elexoft on 19/02/2025.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
