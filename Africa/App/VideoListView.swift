7295b6a7ab5d6565f03b66218b45c0bd6d5efc1a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 14:52:24 +0500

    Shuffle is added o the project and make the data state and also add the feednack generator of imapct and add it to the shiffle button

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct VideoListView: View {
     // MARK: - PROPERTIES
-    let videos: [Video] = Bundle.main.decode("videos.json")
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - BODY
     
@@ -23,6 +25,17 @@ struct VideoListView: View {
             } //: LIST
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Shuffle Videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
         } //: NAVIGATION
     }
 }
