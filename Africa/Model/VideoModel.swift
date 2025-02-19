8187106c644a1ac23c75f993c359b83d54ccbb59
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-19 14:10:11 +0500

    Video model file is acreated

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Elexoft on 19/02/2025.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
