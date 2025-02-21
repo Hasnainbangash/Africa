932e801adc03bce44620fbd7f30022d8f5b3015f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 10:18:51 +0500

    wrap the text in a zstack and in zstack use the cirlc ena dmake it postion to the center to make it in the center

--- Africa/View/MotionAnimationView.swift
+++ Africa/View/MotionAnimationView.swift
@@ -17,7 +17,18 @@ struct MotionAnimationView: View {
     var body: some View {
         // With this geometry reader container we can measure not only the screen size but any view size, Basically it gives us access to the size and position of the parent view. One important thing we should keep in mind that when we wrap any view into a geometry reader container than the origin point will be changed from the center to top left
         GeometryReader {geometry in
-            Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
+            ZStack {
+                Circle()
+                    .foregroundColor(.gray)
+                    .opacity(0.15)
+                    .frame(width: 256, height: 256, alignment: .center)
+                    .position(
+                        x: geometry.size.width / 2,
+                        y: geometry.size.height / 2
+                    )
+                
+                Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
+            } //: ZSTACK
         } //: Geometry
     }
 }
