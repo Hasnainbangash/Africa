03759b4f8ce6fa81f59c1381cf7387418dbb18d5
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 11:57:31 +0500

    Anmaton is added

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -67,6 +67,7 @@ struct GalleryView: View {
                             }
                     } //: LOOP
                 } //: GRID
+                .animation(.easeIn)
                 .onAppear {
                     gridSwitch()
                 }
