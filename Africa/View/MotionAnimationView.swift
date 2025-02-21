618b6a519466b0b0a9ce91723a2afec24dee7d2e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 10:22:27 +0500

    RandomCircle numbers are genrated and make the circle in repeat loop to make it more than one

--- Africa/View/MotionAnimationView.swift
+++ Africa/View/MotionAnimationView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MotionAnimationView: View {
     // MARK: - PROPERTIES
     
-    
+    @State private var randomCircle = Int.random(in: 12...16)
     
     // MARK: - BODY
     
@@ -18,14 +18,16 @@ struct MotionAnimationView: View {
         // With this geometry reader container we can measure not only the screen size but any view size, Basically it gives us access to the size and position of the parent view. One important thing we should keep in mind that when we wrap any view into a geometry reader container than the origin point will be changed from the center to top left
         GeometryReader {geometry in
             ZStack {
-                Circle()
-                    .foregroundColor(.gray)
-                    .opacity(0.15)
-                    .frame(width: 256, height: 256, alignment: .center)
-                    .position(
-                        x: geometry.size.width / 2,
-                        y: geometry.size.height / 2
-                    )
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: 256, height: 256, alignment: .center)
+                        .position(
+                            x: geometry.size.width / 2,
+                            y: geometry.size.height / 2
+                        )
+                } //: LOOP
                 
                 Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
             } //: ZSTACK
