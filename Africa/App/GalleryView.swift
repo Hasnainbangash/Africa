34f69e631cd0b3d386d5bb4b683ea16841cee712
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-21 11:08:46 +0500

    lazyVGrid is added to the project

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct GalleryView: View {
     // MARK: - PROPERTIES
     
-    
+    // SIMPLE GRID DEFINATION
+    let gridLayout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     // MARK: - BODY
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            // MARK: - GRID
+            
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                Text("Gallery")
+            } //: GRID
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
